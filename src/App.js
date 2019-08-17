530940bc05fff81ca0516faea6ecef9c9b328855
gsandhu916 <gsandhu916@gmail.com>
2019-08-16 23:06:18 -0700

    added and setState for all of filter side

--- src/App.js
+++ src/App.js
@@ -1,33 +1,68 @@
-import React, { useState} from 'react';
-import Header from './components/Header';
+import React, { useState } from "react";
+import Header from "./components/Header";
 import "./scss/App.scss";
-import Filter from './components/Filter';
-import Listings from './components/Listings';
-import listingData from './data/listingData';
+import Filter from "./components/Filter";
+import Listings from "./components/Listings";
+import listingData from "./data/listingData";
 
 const App = () => {
   let data = listingData;
-  let [filterState, setFilterState] = useState([])
+  let [filterState, setFilterState] = useState({});
+  let [min_price, setmin_price] = useState(0);
+  let [max_price, setmax_price] = useState(10000000);
+  let [min_floor_space, setmin_floor_space] = useState(100);
+  let [max_floor_space, setmax_floor_space] = useState(50000);
+  let [elevator, setElevator] = useState(false);
+  let [swimming_pool, setSwimming_pool] = useState(false);
+  let [finished_basement, setFinished_basement] = useState(false);
+  let [gym, setGym] = useState(false);
+  // let globalState = ['Ridgewood', 'Ranch', '1BR', min_price, max_price, min_floor_space, max_floor_space, elevator, swimming_pool, finished_basement, gym]
 
-  console.log(data);
-  const change = (event) => {
-    event.preventDefault()
+  // console.log(globalState);
+  const change = event => {
     let name = event.target.name;
-    let value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
-    setFilterState({[name]: value})
-    // console.log(event.target.name, event.target.value)
-  }
-console.log(filterState)
-	return (
-		<div>
-			<Header />
-			<section id="content-area">
-				<Filter change={change}/>
-				<Listings data={data}/>
-			</section>
-		</div>
-	);
-};
+    let value =
+      event.target.type === "checkbox"
+        ? event.target.checked
+        : event.target.value;
+    console.log(name);
+    setFilterState({
+      ...filterState,
+      [name]: value
+    });
+    setmin_price(name === "min_price" ? value : min_price);
+    setmax_price(name === "max_price" ? value : max_price);
+    setmin_floor_space(name === "min_floor_space" ? value : min_floor_space);
+    setmax_floor_space(name === "max_floor_space" ? value : max_floor_space);
+    setElevator(name === "elevator" ? value : elevator);
+    setSwimming_pool(name === "swimming_pool" ? value : swimming_pool);
+    setFinished_basement(
+      name === "finished_basement" ? value : finished_basement
+    );
+    setGym(name === "gym" ? value : gym);
+  };
 
+  console.log(filterState);
+  return (
+    <div>
+      <Header />
+      <section id="content-area">
+        <Filter
+          change={change}
+          min_price={min_price}
+          max_price={max_price}
+          min_floor_space={min_floor_space}
+          max_floor_space={max_floor_space}
+          elevator={elevator}
+          swimming_pool={swimming_pool}
+          finished_basement={finished_basement}
+          gym={gym}
+          // globalState={globalState}
+        />
+        <Listings data={data} />
+      </section>
+    </div>
+  );
+};
 
 export default App;
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,68 +1,115 @@
-import React from 'react';
+import React from "react";
 
-const Filter = ({change}) => {
-	return (
-		<section id="filter">
-			<div className="inside">
-				<h4>Filter</h4>
-				<select onChange={change} name="neighbourhood" className="filters neighbourhood">
-					<option value='Ridgewood'>Ridgewood</option>
-					<option value='Miami'>Miami</option>
-				</select>
-				<select onChange={change} name="housetype" className="filters housetype">
-					<option>Ranch</option>
-					<option>House</option>
-					<option>Apartment</option>
-				</select>
-				<select onChange={change} name="bedrooms" className="filters bedrooms">
-					<option>1 BR</option>
-					<option>2 BR</option>
-					<option>3 BR</option>
-					<option>4 BR</option>
-					<option>5 BR</option>
-				</select>
-				<div className="filters price">
-					<span className="title">Price</span>
-					<input onChange={change} type="text" name="min-price" className="min-price" />
-					<input onChange={change} type="text" name="max-price" className="max-price" />
-				</div>
-				<div className="filters floor-space">
-					<span className="title">Floor Space</span>
-					<input
+const Filter = ({
+  change,
+  min_price,
+  max_price,
+  min_floor_space,
+  max_floor_space,
+  elevator,
+  swimming_pool,
+  finished_basement,
+  gym
+}) => {
+  return (
+    <section id="filter">
+      <div className="inside">
+        <h4>Filter</h4>
+        <select
           onChange={change}
-						type="text"
-						name="min-floor-space"
-						className="min-floor-space"
-					/>
-					<input
+          name="neighbourhood"
+          className="filters neighbourhood"
+        >
+          <option value="Ridgewood">Ridgewood</option>
+          <option value="Miami">Miami</option>
+        </select>
+        <select
           onChange={change}
-						type="text"
-						name="max-floor-space"
-						className="max-floor-space"
-					/>
-				</div>
-				<div className="filters extras">
-					<span className="title">Extras</span>
-					<label htmlFor="extras">
-						<span>Elevators</span>
-						<input onChange={change} name="extras" value="elevator" type="checkbox" />
-					</label>
-					<label htmlFor="extras">
-						<span>Swimming Pool</span>
-						<input onChange={change} name="extras" value="swimming-pool" type="checkbox" />
-					</label>
-					<label htmlFor="extras">
-						<span>Finished Basement</span>
-						<input onChange={change} name="extras" value="finished-basement" type="checkbox" />
-					</label>
-					<label htmlFor="extras">
-						<span>Gym</span>
-						<input onChange={change} name="extras" value="gym" type="checkbox" />
-					</label>
-				</div>
-			</div>
-		</section>
-	);
+          name="housetype"
+          className="filters housetype"
+        >
+          <option>Ranch</option>
+          <option>House</option>
+          <option>Apartment</option>
+        </select>
+        <select onChange={change} name="bedrooms" className="filters bedrooms">
+          <option>1 BR</option>
+          <option>2 BR</option>
+          <option>3 BR</option>
+          <option>4 BR</option>
+          <option>5 BR</option>
+        </select>
+        <div className="filters price">
+          <span className="title">Price</span>
+          <input
+            onChange={change}
+            type="text"
+            name="min_price"
+            className="min_price"
+            value={min_price}
+          />
+          <input
+            onChange={change}
+            type="text"
+            name="max_price"
+            className="max_price"
+            value={max_price}
+          />
+        </div>
+        <div className="filters floor-space">
+          <span className="title">Floor Space</span>
+          <input
+            onChange={change}
+            type="text"
+            name="min_floor_space"
+            className="min_floor_space"
+            value={min_floor_space}
+          />
+          <input
+            onChange={change}
+            type="text"
+            name="max_floor_space"
+            className="max_floor_space"
+            value={max_floor_space}
+          />
+        </div>
+        <div className="filters extras">
+          <span className="title">Extras</span>
+          <label htmlFor="ele">
+            <span>Elevators</span>
+            <input
+              onChange={change}
+              name="elevator"
+              value={elevator}
+              type="checkbox"
+            />
+          </label>
+          <label htmlFor="extras">
+            <span>Swimming Pool</span>
+            <input
+              onChange={change}
+              name="swimming_pool"
+              value={swimming_pool}
+              type="checkbox"
+            />
+          </label>
+          <label htmlFor="extras">
+            <span>Finished Basement</span>
+            <input
+              onChange={change}
+              name="finished_basement"
+              value={finished_basement}
+              type="checkbox"
+            />
+          </label>
+          <label htmlFor="extras">
+            <span>Gym</span>
+            <input onChange={change} name="gym" value={gym} type="checkbox" />
+          </label>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Filter;
