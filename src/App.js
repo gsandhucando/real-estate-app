771a836701bd35376a2cc8c42c78fc0804b9f156
gsandhu916 <gsandhu916@gmail.com>
2019-08-19 20:12:52 -0700

    added footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
+import Footer from './components/Footer';
 import "./scss/App.scss";
 import Filter from "./components/Filter";
 import Listings from "./components/Listings";
@@ -261,6 +262,7 @@ const App = () => {
           changeViewLong={() => changeViewLong}
         />
       </section>
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { Icon } from '@iconify/react';
+import googlePlay from '@iconify/icons-fa-brands/google-play';
+import appleFill from '@iconify/icons-ant-design/apple-fill';
+import facebookIcon from '@iconify/icons-fa-brands/facebook';
+import twitterCircleFill from '@iconify/icons-ant-design/twitter-circle-fill';
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className='footer-container'>
+      {/* <img /> */}
+      <div className='footer-left'>
+      <div className='app-writing'>
+      <h1>Download Properties App</h1>
+      <p>Use your app to have access to all our rental properties</p>
+      </div>
+      <div className='footer-btn-container'>
+      <a href="#-" className="footer-btn"><Icon icon={googlePlay} className='icon' /> Playstore</a>
+      <a href="#-" className="footer-btn"><Icon icon={appleFill} className='icon' /> AppStore</a>
+      </div>
+      <div className='social-media'>
+      <Icon icon={facebookIcon} className='facebook' />
+      <Icon icon={twitterCircleFill} className='twitter' />
+      </div>
+      <h5>2019 Properties</h5>
+      </div>
+      <div className='footer-right'>
+      <div className='tentants'>
+        <h3>For Tentants</h3>
+        <p>About Properties</p>
+        <p>Sign Up for free</p>
+        <p>This is us</p>
+        <p>Properties APP</p>
+      </div>
+      <div className='advertisers'>
+        <h3>For Advertisers</h3>
+        <p>About Properties</p>
+      </div>
+      </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
